938419b8241f02a0b4481144a20d401a89d10bb4
channelltom <channelltom@gmail.com>
2023-12-12 20:16:20 +1100

    initial git commit

---
+++ script.js
@@ -0,0 +1,19 @@
+const add = function (val1, val2) {
+  return val1 + val2;
+};
+
+const subtract = function (val1, val2) {
+  return val1 - val2;
+};
+
+const multiply = function (val1, val2) {
+  return val1 * val2;
+};
+
+const divide = function (val1, val2) {
+  return val1 / val2;
+};
+
+const operate = function (operator, value1, value2) {
+  return add(value1, value2);
+};
